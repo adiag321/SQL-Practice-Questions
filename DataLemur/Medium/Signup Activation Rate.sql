1d25cf33222b5b42d57674ae98115f1baffa4e95
Aditya Agarwal <adi.ag321@gmail.com>
2026-06-04 09:59:38 -0500

    delete files

--- DataLemur/Medium/Signup Activation Rate.sql
+++
@@ -1,9 +0,0 @@
-SELECT ROUND
-       (
-          SUM(CASE WHEN signup_action = 'Confirmed' THEN 1.0 ELSE 0.0 END) / 
-          COUNT(emails.email_id)
-          , 2
-       )
-FROM texts
-LEFT JOIN emails
-USING(email_id) 
--- DataLemur/Medium/User's Third Transaction.sql
+++
@@ -1,7 +0,0 @@
-SELECT user_id, spend, transaction_date
-FROM (
-      SELECT *,ROW_NUMBER () OVER (PARTITION BY user_id ORDER BY transaction_date) AS RD
-      FROM transactions
-     ) AS T
-     
-WHERE RD = 3
